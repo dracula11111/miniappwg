0f5b465cd1f35b2f3539c216c5f2616b727de013
kiruhadev <kirillcimbalaru@gmail.com>
2026-01-17 17:46:40 +0100

    54

--- server.js
+++ server.js
@@ -171,8 +171,7 @@ const db = IS_TEST ? dbMem : dbReal;
 
 const app = express();
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
