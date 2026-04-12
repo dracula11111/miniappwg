23b38dbe6d0b620af390290a54acd74cb9827f11
dracula11111 <kirilhram29@gmail.com>
2026-04-12 20:13:25 +0200

    Revert "wheel perf/ fixes"
    This reverts commit 187a7a4a87d82648cd7cc54d7557bd6ecf4f3f78.

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,6 @@ import "../css/unauthorized.css";
 
 import "./core/perf.js";
 import { blockNonTelegramBrowserInProd } from "./shared/telegram-entry-guard.js";
-import "./core/perf.js";
 
 // Mount the large HTML shell into #root before feature scripts run.
 import "./app-shell.js";
